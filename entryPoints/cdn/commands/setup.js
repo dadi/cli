788ddddf151d856f42cd56b51065eeb78da137c9
Eduardo Bouças <mail@eduardoboucas.com>
2018-12-05 13:35:31 +0000

    feat: add error message to CDN setup command

--- entryPoints/cdn/commands/setup.js
+++ entryPoints/cdn/commands/setup.js
@@ -3,6 +3,7 @@
 const configHelpers = require('./../../../lib/config')
 const fsHelpers = require('./../../../lib/fs')
 const Setup = require('./../../../lib/setup')
+const shellHelpers = require('./../../../lib/shell')
 
 const steps = [
   // Server
@@ -291,6 +292,13 @@ const launchSetup = () => {
       description: 'CDN configuration file',
       fileName: `config.${answers.env}.json`
     })
+  }).catch(error => {
+    shellHelpers.showSpinner(
+      'This directory does not seem to contain a valid installation of DADI CDN.',
+      'fail'
+    )
+
+    return Promise.reject(error)
   })
 }
 
